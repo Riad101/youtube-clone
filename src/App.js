4fb3cd51f721cd47723290e3353a2bdd01e82186
sm riad <riad@abatacommerce.com>
2021-11-02 17:06:26 +0800

    second commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,34 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import {Grid} from '@material-ui/core';
+import youtube from "./api/youtube";
+import {SearchBar, VideoDetail, VideoList} from './components'
+
+class App extends React.Component{
+    render(){
+        return(
+            <Grid justify="center" container spacing={16}>
+                <Grid items xs={12}>
+                    <Grid container spacing={16}>
+                        <Grid item xs={12}>
+                            <SearchBar />
+
+                        </Grid>
+                        <Grid item xs={8}>
+                            <VideoDetail />
+
+                        </Grid>
+                        <Grid item xs={4}>
+                            {/* video list */}
+                        </Grid>
+
+                    </Grid>
+
+                </Grid>
+
+            </Grid>
+        )
+    }
 }
 
 export default App;
